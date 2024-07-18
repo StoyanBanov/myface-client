02abf9ba57252edef0497fd9ca1b8ac57f75f322
Stoyan Banov <sbanov05@gmail.com>
2024-07-18 17:22:32 +0300

    add localStorage session functionality

---
+++ src/util/session.js
@@ -0,0 +1,5 @@
+export const getAuthData = () => JSON.parse(localStorage.getItem('auth')) || {}
+
+export const setAuthData = (data) => localStorage.setItem('auth', JSON.stringify(data))
+
+export const clearAuthData = () => localStorage.removeItem('auth')
