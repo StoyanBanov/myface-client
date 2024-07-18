840e0670b2a6d2964ffc0aafe9d6f57bb37067dc
Stoyan Banov <sbanov05@gmail.com>
2024-07-18 17:48:38 +0300

    add dependencies for react-router adn bump main

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+
+import { Provider } from 'react-redux'
+
 import App from './App.jsx'
+import store from './store/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    //<React.StrictMode>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
+    //</React.StrictMode>,
 )
