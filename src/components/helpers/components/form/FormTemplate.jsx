a3cc04aaca8ee4c83de13447a00745b92a61e855
Stoyan Banov <sbanov05@gmail.com>
2024-08-07 20:47:44 +0300

    bump formTemplate component to include optional preloader

--- src/components/helpers/components/form/FormTemplate.jsx
+++ src/components/helpers/components/form/FormTemplate.jsx
@@ -1,16 +1,21 @@
+import Loading from '../preload/Loading'
 import style from './style.module.css'
 
-const FormTemplate = ({ children, onSubmit, title, btnTxt }) => {
+const FormTemplate = ({ children, onSubmit, title, btnTxt, preload = false }) => {
     return (
-        <fieldset className={style.mainFieldset}>
-            <legend><h2>{title}</h2></legend>
+        <>
+            <fieldset className={style.mainFieldset}>
+                <legend><h2>{title}</h2></legend>
 
-            <form className={style.mainForm} onSubmit={onSubmit} >
-                {children}
+                <form className={style.mainForm} onSubmit={onSubmit} >
+                    {children}
 
-                <button>{btnTxt}</button>
-            </form>
-        </fieldset>
+                    <button>{btnTxt}</button>
+                </form>
+
+                <Loading loading={preload} />
+            </fieldset>
+        </>
     )
 }
 
