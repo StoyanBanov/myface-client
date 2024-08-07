4d977835e55b17f7a4bbf16b7d5a0c9e5895e416
Stoyan Banov <sbanov05@gmail.com>
2024-08-07 20:48:34 +0300

    bump carousel ux

--- src/components/helpers/components/images/Carousel.jsx
+++ src/components/helpers/components/images/Carousel.jsx
@@ -4,8 +4,8 @@ import style from './style.module.css'
 import { CDN_ADDRESS } from '../../../../constants'
 import SideArrowSvg from '../svgs/SideArrowSvg'
 
-const Carousel = ({ imgIds, closeHandler }) => {
-    const [index, setIndex] = useState(0)
+const Carousel = ({ imgIds, ind, closeHandler }) => {
+    const [index, setIndex] = useState(ind)
 
     useEffect(() => {
         document.body.style.overflow = 'hidden'
@@ -29,13 +29,13 @@ const Carousel = ({ imgIds, closeHandler }) => {
 
     return (
         <div className={style.carouselContainer}>
-            <button onClick={onLeftClick}><SideArrowSvg width={40} height={90} side='left' strokeWidth={3} /></button>
+            <button onClick={onLeftClick}><SideArrowSvg width={30} height={70} side='left' strokeWidth={3} /></button>
 
             <span className={style.closeBtn} onClick={onClose}><CloseSvg /></span>
 
             <img src={`${CDN_ADDRESS}/${imgIds[index]}`} />
 
-            <button onClick={onRightClick}><SideArrowSvg width={40} height={90} side='right' strokeWidth={3} /></button>
+            <button onClick={onRightClick}><SideArrowSvg width={30} height={70} side='right' strokeWidth={3} /></button>
         </div>
     )
 }
