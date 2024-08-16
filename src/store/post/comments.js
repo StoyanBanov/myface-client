1818a847a372ead19eb9ed83ec18f114a508f907
Stoyan Banov <sbanov05@gmail.com>
2024-08-16 21:02:18 +0300

    add comment replies to store

--- src/store/post/comments.js
+++ src/store/post/comments.js
@@ -43,6 +43,67 @@ const comments = createSlice({
             state.loading = false
         },
 
+
+        repliesReceived: (state, action) => {
+            const { parents } = action.payload
+
+            let comment = state.list.find(c => c._id == parents.pop())
+
+            while (parents.length) {
+                const parent = parents.pop()
+                comment = comment.replies.find(c => c._id == parent)
+            }
+
+            const replies = action.payload.data
+
+            if (comment.replies) {
+                comment.replies.unshift(...replies)
+                comment.repliesSkip += replies.length
+            } else {
+                comment.replies = replies
+                comment.repliesSkip = replies.length
+            }
+
+            state.loading = false
+        },
+        replyAdded: (state, action) => {
+            const { parents } = action.payload
+
+            let comment = state.list.find(c => c._id == parents.pop())
+
+            while (parents.length) {
+                const parent = parents.pop()
+                comment = comment.replies.find(c => c._id == parent)
+            }
+
+            const reply = action.payload.data
+
+            if (comment.replies) {
+                comment.replies.unshift(reply)
+                comment.repliesSkip++
+            } else {
+                comment.replies = [reply]
+                comment.repliesSkip = 1
+            }
+
+            state.loading = false
+        },
+        replyRemoved: (state, action) => {
+            const { parents } = action.payload
+
+            let comment = state.list.find(c => c._id == parents.pop())
+
+            while (parents.length) {
+                const parent = parents.pop()
+                comment = comment.replies.find(c => c._id == parent)
+            }
+
+            comment.replies.splice(comment.replies.findIndex(c => c._id == action.payload.comment), 1)
+            comment.repliesSkip--
+
+            state.loading = false
+        },
+
         cleared: () => comments.getInitialState()
     }
 })
@@ -59,16 +120,20 @@ const {
     added,
     removed,
 
+    repliesReceived,
+    replyAdded,
+    replyRemoved,
+
     cleared
 } = comments.actions
 
 
 // Action Creators
-export const getComments = (postId) =>
+export const getComments = (post) =>
     (dispatch, getState) => {
         dispatch(
             apiCallBegan({
-                url: `${url}/${postId}?orderBy=${encodeURIComponent('"createdAt=-1"')}&skip=${getState().entities.comments.skip}`,
+                url: `${url}?where=${encodeURIComponent(`"post=${post}"`)}&orderBy=${encodeURIComponent('"createdAt=-1"')}&skip=${getState().entities.comments.skip}`,
                 onStart: requested.type,
                 onSuccess: received.type,
                 onError: requestFailed.type
@@ -94,6 +159,37 @@ export const deleteCommentById = (comment) =>
         onSuccess: removed.type,
         onError: requestFailed.type,
         persist: { comment }
+    })
+
+
+export const getReplies = (commentObj, parents) =>
+    apiCallBegan({
+        url: `${url}?where=${encodeURIComponent(`"comment=${commentObj._id}"`)}&orderBy=${encodeURIComponent('"createdAt=-1"')}&skip=${commentObj.skip}`,
+        onStart: requested.type,
+        onSuccess: repliesReceived.type,
+        onError: requestFailed.type,
+        persist: { parents }
+    })
+
+export const addReply = (body, parents) =>
+    apiCallBegan({
+        url,
+        method: 'post',
+        body,
+        onStart: requested.type,
+        onSuccess: replyAdded.type,
+        onError: requestFailed.type,
+        persist: { parents }
+    })
+
+export const deleteReply = (comment, parents) =>
+    apiCallBegan({
+        url: `${url}/${comment}`,
+        method: 'delete',
+        onStart: requested.type,
+        onSuccess: replyRemoved.type,
+        onError: requestFailed.type,
+        persist: { parents }
     })
 
 export const clearComments = () => cleared()
